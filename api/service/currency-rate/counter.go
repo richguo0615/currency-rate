478153e7f6e517cba1ca25e46f76086e3fd9a031
Rich Guo <richguo0615@gmail.com>
2022-01-09 16:56:53 +0800

    update: rate - 千分位

--- api/service/currency-rate/counter.go
+++ api/service/currency-rate/counter.go
@@ -3,13 +3,19 @@ package currency_rate
 import (
 	"github.com/richguo0615/currency-rate/api/model"
 	"github.com/richguo0615/currency-rate/tools"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
-func CountRate(rateMap model.CurrencyRateMap, req RateReq) (float64, error) {
+func CountRate(rateMap model.CurrencyRateMap, req RateReq) (string, error) {
 	rateList, _ := rateMap[req.From]
 	rate, _ := rateList[req.To]
 
+	// 四捨五入
 	res := tools.Round(req.Amount * rate, 2)
 
-	return res, nil
+	// 千分位
+	p := message.NewPrinter(language.English)
+
+	return p.Sprintf("%.2f", res), nil
 }
